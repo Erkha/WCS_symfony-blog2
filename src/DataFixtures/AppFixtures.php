fdb775edf77a188dd33ef44cb2278ccb731960c4
Erkha <jmtatout@gmail.com>
2019-06-14 09:36:36 +0200

    added an article fixture in order to have a big DB

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,48 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Service\Slugify;
+use App\Entity\Category;
+use App\Entity\Tag;
+use App\Entity\Article;
 
 class AppFixtures extends Fixture
 {
+		/**
+		* @var Slugify
+		*/
+		private $slugify;
+
+		/**
+		* AppFixtures constructor.
+		* @param Slugify $slugify
+		*/
+	
+		public function __construct(Slugify $slugify)
+		{
+    	$this->slugify = $slugify;
+		}
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 1; $i <= 1000; $i++) {
+				   $category = new Category();
+				   $category->setName("category " . $i);
+				   $manager->persist($category);
 
-        $manager->flush();
+				   $tag = new Tag();
+				   $tag->setName("tag " . $i);
+				   $manager->persist($tag);
+
+				   $article = new Article();
+				   $article->setTitle("article " . $i);
+				   $article->setSlug($this->slugify->generate($article->getTitle()));
+				   $article->setContent("article " . $i . " content");
+				   $article->setCategory($category);
+				   $article->addTag($tag);
+				   $manager->persist($article);
+				}
+				$manager->flush();
     }
+
 }
